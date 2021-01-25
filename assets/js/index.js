ea792914e01280ad78e32da1a1382b8394bfffad
lzw <811450713@qq.com>
2021-01-25 15:32:20 +0800

    主页的登录退出已完成

---
+++ assets/js/index.js
@@ -0,0 +1,63 @@
+// 发送ajax请求获取用户信息
+
+function getUserInfo() {
+  axios.get('/my/userinfo',{
+    //  headers: {
+    //     Authorization: localStorage.getItem("token"),
+    //   }, 
+  }).then((res) => {
+    // console.log(res);
+    if(res.data.status !== 0 ){
+      // 说明获取失败
+      return layer.msg('获取用户信息失败')
+    }
+    // 获取用户信息成功 ==> 处理头像昵称
+    avatarAndName(res.data)
+  })
+  // axios({
+  //   url:'/my/userinfo',
+  //   headers:{
+  //     Authorization: localStorage.getItem('token')
+  //   }
+  // }).then((res)=>{console.log(res);})
+}
+getUserInfo()
+
+function avatarAndName(res){
+  // ==> 处理头像昵称
+  // console.log(res);
+  // 处理名字优先展示nickname
+  let name = res.data.nickname || res.data.username
+  // 修改名字
+  $('#welcome').text('欢迎 '+name)
+  // 处理头像
+  if (res.data.user_pic) { // 如果头像的值不为空就是有上传头像
+    // 如果有上传头像则显示上传的头像,没有就显示文字头像
+    $('.layui-nav-img').attr('src',res.data.user_pic).show()
+    $('.text_avatar').hide()
+  }else{
+    // 如果没有上传头像则显示文字的头像,
+    let first = name[0].toUpperCase()
+    $('.text_avatar').text(first).show()
+    // $('.layui-nav-img').hide()
+  }
+}
+// ===================== 退出 ==================
+$('#logoutBtn').on('click',function(){
+  // 弹窗提示是否退出
+  layer.confirm('确定退出吗?', {icon: 3, title:'提示'}, function(index){
+    // 退出要做的事
+    // 将本地存储的token删除掉
+    localStorage.removeItem('token')
+    // 跳转页面
+    location.href = '/home/login.html'
+    layer.close(index);
+  });
+})
+
+
+
+
+
+
+
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,14 +1,14 @@
 
-;$(function(){
-// 注册账号
-  $('#gotoRegi').on('click',function(){
+; $(function () {
+  // 注册账号
+  $('#gotoRegi').on('click', function () {
     // 显示注册界面
     $('.regiBox').show()
     // 隐藏登录界面
     $('.loginBox').hide()
   })
   // 去登录
-  $('#gotoLogin').on('click',function(){
+  $('#gotoLogin').on('click', function () {
     // 显示注册界面
     $('.regiBox').hide()
     // 隐藏登录界面
@@ -20,24 +20,24 @@
   let form = layui.form;
   // 密码验证
   form.verify({
-    repass:function(value,item){
+    repass: function (value, item) {
       // console.log(value);
       let pwd = $('.regiBox [name=password]').val()
       // console.log(pwd);
-      if(value !== pwd){
+      if (value !== pwd) {
         return '两次密码输入不一致'
       }
     }
     //我们既支持上述函数式的方式，也支持下述数组的形式
     //数组的两个值分别代表：[正则匹配、匹配不符时的提示文字]
-    ,pass: [
+    , pass: [
       /^[\S]{6,12}$/
-      ,'密码必须6到12位，且不能出现空格'
-    ] 
-  });   
+      , '密码必须6到12位，且不能出现空格'
+    ]
+  });
 
   //==========================  注册 =========================
- 
+
 
   // $('.regiBox form').on('submit',function(e){
   //   e.preventDefault()
@@ -57,15 +57,15 @@
   //   })
   // })
 
-  $('.regiBox form').on('submit',function(e){
+  $('.regiBox form').on('submit', function (e) {
     e.preventDefault()
     // console.log($(this).serialize());
     let data = $(this).serialize() // 拿到form表单里面的name属性的值
-    axios.post('/api/reguser',data).then((res)=>{
+    axios.post('/api/reguser', data).then((res) => {
       console.log(res);
-      if (res.data.status===1) {
+      if (res.data.status === 1) {
         return layer.msg(res.data.message)
-      }else{
+      } else {
         layer.msg('注册成功!')
         $('#gotoLogin').click()
       }
@@ -73,23 +73,25 @@
   })
   //========================== 登录 ============================
 
-  $('.loginBox form').on('submit',function(e){
+  $('.loginBox form').on('submit', function (e) {
     e.preventDefault()
     let data = $(this).serialize()
-    axios.post('/api/login',data).then(function(res){
-      if(res.data.status!==0){
-        return layer.msg(res.data.message+'账号或密码错误')
-      }else{
-        //登陆成功
-        layer.msg('登录成功,即将跳转到首页',{time: 2000},function(){
-          // 跳转页面
-          location.href = '/home/index.html'
-        })
-        // 需要本地存储====>获取服务器响应回来的token信息(随机码),方便后期使用token
-        localStorage.setItem('token',res.data.token)
+    axios.post('/api/login', data).then(function (res) {
+      console.log(res);
+      // layer.msg(res.data.message+'账号或密码错误')
+      if (res.data.status !== 0) {    // 登录失败
+        // console.log(1111);
+        return layer.msg(res.data.message + '账号或密码错误')
       }
+      //登陆成功
+      layer.msg('登录成功,即将跳转到首页', { time: 2000 }, function () {
+        // 跳转页面
+        // location.href = '/home/index.html'
+      })
+      // 需要本地存储====>获取服务器响应回来的token信息(随机码),方便后期使用token
+      localStorage.setItem('token', res.data.token)
     })
-  })  
+  })
 })
 
 
